2ce1c8c2c3dd1e3170a8104402a975d3f86b9142
VasilyeuUV <vasilyeu.uv@mail.ru>
2026-09-18 22:52:15 +0300

    feat: add menu page, add head component, refactor components.js.

--- coffee-house/js/components.js
+++ coffee-house/js/components.js
@@ -1,26 +1,76 @@
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded', loadComponents);
+
+/**
+ * Асинхронная загрузка компонентов.
+ */
+async function loadComponents() {
     const components = document.querySelectorAll('[data-component]');
 
-    for (const element of components) {
-        const url = element.dataset.component;
-
-        try {
-            const response = await fetch(url);
-
-            if (!response.ok) {
-                throw new Error(
-                    `Failed to load component: ${url} (${response.status})`
-                );
-            }
-
-            const html = await response.text();
-
-            const template = document.createElement('template');
-            template.innerHTML = html.trim();
-
-            element.replaceWith(template.content);
-        } catch (error) {
-            console.error(error);
-        }
+    for (const component of components) {
+        await loadComponent(component);
     }
-});
+}
+
+
+/**
+ * Загрузить конкретный компонент и определить его тип.
+ * 
+ * @param {string} element - Найденный компонент
+ * @returns 
+ */
+async function loadComponent(element) {
+    const url = element.dataset.component;
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(
+                `Failed to load component: ${url} (${response.status})`
+            );
+        }
+
+        const html = await response.text();
+
+        if (element.tagName === 'HEAD') {
+            loadHead(element, html);
+            return;
+        }
+
+        replaceComponent(element, html);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+/**
+ * Вставить компонент в блок <head> c <title>
+ * 
+ * @param {string} head - Блок <head> для вставки компонента.
+ * @param {string} html - вставляемый код. 
+ */
+function loadHead(head, html) {
+    head.innerHTML = html;
+
+    const title = head.querySelector('title');
+    const pageTitle = head.dataset.pageTitle;
+
+    if (title && pageTitle) {
+        title.textContent = pageTitle;
+    }
+}
+
+
+/**
+ * Вставить компонент в блок <body> с заменой.
+ * 
+ * @param {string} element - элемент компонента.
+ * @param {string} html - вставляемый код компонента.
+ */
+function replaceComponent(element, html) {
+    const template = document.createElement('template');
+    template.innerHTML = html.trim();
+
+    element.replaceWith(template.content);
+}
